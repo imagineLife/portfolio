e0e8ea8de5078e04475354c6ac2b88c3ff46cee6
Eric Laursen <ericjlaursen@gmail.com>
2018-01-22 19:19:39 -0500

    Includes hider js on close click for modal.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log('Dom Element',document.getElementById('modal'));
+
+function toggleModal(){
+    let modal = document.getElementById('modal')
+    let modalStyle = getComputedStyle(modal)['display'];
+
+    modal.style.display = ( modalStyle === 'block' ) ? 'none' : 'block';
+}

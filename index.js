5a66a4951e7182ebb6f49ba4f9b10d5ef2cfbf90
Eric Laursen <ericjlaursen@gmail.com>
2018-01-24 20:46:10 -0500

    Changed image names in erp and builds console.log on image click.

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-function toggleModal(){
+function getCurImgFilePath(imageClicked){
+    console.log('curImgFile',imageClicked.target);
+}
+
+function toggleModal(imageLoc){
     let modal = document.getElementById('modal')
     let modalStyle = getComputedStyle(modal)['display'];
 
-    modal.style.display = ( modalStyle === 'block' ) ? 'none' : 'block';
+    //couldn't figure out adding the fn in ternary
+    //modal.style.display = ( modalStyle === 'block' ) ? 'none' : 'block';
+    
+    //modal.style.display = ( modalStyle === 'block' ) ? getCurImgFilePath() : 'block';
+
+    if(modalStyle === 'none'){
+    	console.log('modal is closed, opening modal');
+    	console.log('imageLoc ->',imageLoc.getAttribute("data-imageloc"));
+    	let curImgFile = document.getElementsByClassName("projectImg");
+
+    	// modal.style.display = 'block';
+
+    }else{
+    	console.log('modal is open, closing modal');
+    	modal.style.display = 'none';
+    }
+
+
 }
